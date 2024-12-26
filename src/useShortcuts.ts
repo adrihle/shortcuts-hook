7ba14a2948af69d3eaeaa0b8b7caabfbf0d87c62
adrian <adrian.lpes@gmail.com>
2024-12-26 18:19:49 +0100

    fix: added jsdoc

--- src/useShortcuts.ts
+++ src/useShortcuts.ts
@@ -44,6 +44,39 @@ const generateShorcutMap = (map: ShortcutConfig['map']): ShortcutMap => {
   }, {} as ShortcutMap);
 };
 
+/**
+ * `useShortcuts` is a custom React hook that listens for specific keyboard shortcuts and triggers 
+ * corresponding actions when those shortcuts are detected. The hook helps in handling keyboard 
+ * interactions within a component for a better user experience.
+ *
+ * @param {Object} config - The configuration object for keyboard shortcuts.
+ * @param {Array} config.map - An array of objects that define the keyboard shortcuts and their actions.
+ * @param {string} config.map[].key - The key for the shortcut (e.g., 'o', 'i', etc.).
+ * @param {Array<string>} config.map[].modifiers - A list of modifier keys that must be pressed for the shortcut to trigger (e.g., 'ctrlKey', 'shiftKey').
+ * @param {Function} config.map[].action - The function to be executed when the shortcut is triggered.
+ * @param {boolean} [config.silent=false] - If true, disables logging actions in the console (default is false).
+ * 
+ * @returns {void} This hook does not return a value.
+ * 
+ * @example config
+ *     map: [
+ *       {
+ *         key: 'o',
+ *         modifiers: ['ctrlKey', 'shiftKey'],
+ *         action: () => console.log('Ctrl + Shift + O pressed')
+ *       },
+ *       {
+ *         key: 'i',
+ *         modifiers: ['altKey', 'ctrlKey'],
+ *         action: () => console.log('Ctrl + Alt + I pressed')
+ *       }
+ *     ],
+ *     silent: false
+ * 
+ * @throws {Error} If the hook is used outside of a functional component.
+ * 
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent} for more on KeyboardEvent.
+ */
 const useShortcuts = ({ map, silent }: ShortcutConfig) => {
   const shortcutMap = useMemo(() => generateShorcutMap(map), [map]);
 
